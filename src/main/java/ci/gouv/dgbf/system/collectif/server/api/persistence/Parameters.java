044285a3651be77439be0e88b108f392964aa886
devlopper <kycdev@gmail.com>
2022-01-05 14:15:52 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/Parameters.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/Parameters.java
@@ -4,6 +4,7 @@ public interface Parameters {
 
 	String BUDGETARY_ACT_IDENTIFIER = "ab_id";
 	String BUDGETARY_ACT_VERSION_IDENTIFIER = "vab_id";
+	String LATEST_BUDGETARY_ACT_VERSION = "vab_dernier";
 	String SECTION_IDENTIFIER = "s_id";
 	String ADMINISTRATIVE_UNIT_IDENTIFIER = "ua_id";
 	String BUDGET_SPECIALIZATION_UNIT_IDENTIFIER = "usb_id";
