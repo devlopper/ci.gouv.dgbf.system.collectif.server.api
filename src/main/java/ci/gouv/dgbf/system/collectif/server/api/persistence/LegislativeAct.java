e4646b3af58abf46c1f17a9c140a395099776d9f
devlopper <kycdev@gmail.com>
2022-01-26 08:47:40 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/LegislativeAct.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/LegislativeAct.java
@@ -1,20 +1,9 @@
 package ci.gouv.dgbf.system.collectif.server.api.persistence;
 
-import java.time.LocalDateTime;
-
 import org.cyk.utility.persistence.entity.IdentifiableSystemScalarStringIdentifiableBusinessStringNamable;
 
 public interface LegislativeAct extends IdentifiableSystemScalarStringIdentifiableBusinessStringNamable {
 
-	String getSignatory();
-	LegislativeAct setSignatory(String signatory);
-	
-	String getReference();
-	LegislativeAct setReference(String reference);
-	
-	LocalDateTime getCreationDate();
-	LegislativeAct setCreationDate(LocalDateTime creationDate);
-	
 	String NAME = "Collectif budgétaire";
 	String NAME_PLURAL = "Collectifs budgétaires";
 }
