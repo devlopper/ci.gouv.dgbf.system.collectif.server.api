c43dc3ce547c65e212871abcfe5205c433549cc8
devlopper <kycdev@gmail.com>
2022-01-31 09:53:47 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/business/LegislativeActVersionBusiness.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/business/LegislativeActVersionBusiness.java
@@ -1,9 +1,12 @@
 package ci.gouv.dgbf.system.collectif.server.api.business;
 
+import org.cyk.utility.business.Result;
 import org.cyk.utility.business.SpecificBusiness;
 
 import ci.gouv.dgbf.system.collectif.server.api.persistence.LegislativeActVersion;
 
 public interface LegislativeActVersionBusiness extends SpecificBusiness<LegislativeActVersion> {
-	
+
+	String CREATE_AUDIT_IDENTIFIER = "CREATION";
+	Result create(String code,String name,Byte number,String legislativeActIdentifier,String auditWho);
 }
