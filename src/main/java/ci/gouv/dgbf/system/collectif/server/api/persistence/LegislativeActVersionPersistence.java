b7da35ab1ccf46961646249807d0f43678e6e924
devlopper <kycdev@gmail.com>
2022-02-08 22:03:49 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/LegislativeActVersionPersistence.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/LegislativeActVersionPersistence.java
@@ -1,8 +1,10 @@
 package ci.gouv.dgbf.system.collectif.server.api.persistence;
 
+import javax.persistence.EntityManager;
+
 import org.cyk.utility.persistence.SpecificPersistence;
 
 public interface LegislativeActVersionPersistence extends SpecificPersistence<LegislativeActVersion> {
 
-	LegislativeActVersion readUsingNamedQueryReadByIdentifier(String identifier);
+	LegislativeActVersion readUsingNamedQueryReadByIdentifier(String identifier,EntityManager entityManager);
 }
