c713f465f96c3531617b9521c5265d1d68c4e384
devlopper <kycdev@gmail.com>
2022-02-14 08:26:25 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/LegislativeActVersion.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/persistence/LegislativeActVersion.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.cyk.utility.persistence.entity.IdentifiableSystemScalarStringIdentifiableBusinessStringNamable;
 
+import lombok.Getter;
+
 public interface LegislativeActVersion extends IdentifiableSystemScalarStringIdentifiableBusinessStringNamable {
 
 	LegislativeAct getAct();
@@ -20,4 +22,50 @@ public interface LegislativeActVersion extends IdentifiableSystemScalarStringIde
 	
 	String NAME = "Version collectif budgétaire";
 	String NAME_PLURAL = "Versions collectifs budgétaires";
+	
+	/**/
+	
+	String PROCESSING_VALUE_EXPENDITURES_IMPORTATION = "IMPORTATION_DEPENSES";
+	String PROCESSING_VALUE_DUPLICATION = "DUPLICATION";
+	String PROCESSING_VALUE_COPY = "COPIE";
+	
+	@Getter
+	public static enum Processing {
+		/**
+		 * This is an act to cancel a previously passed act
+		 */
+		EXPENDITURES_IMPORTATION(PROCESSING_VALUE_EXPENDITURES_IMPORTATION),
+		/**This is an act to reposition a cancelled act
+		 * 
+		 */
+		DUPLICATION(PROCESSING_VALUE_DUPLICATION),
+		/**
+		 * This is an act to consider made adjustments
+		 */
+		COPY(PROCESSING_VALUE_COPY)
+		
+		;
+		
+		private String value;
+		
+		private Processing(String value) {
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return value;
+		}
+		
+		/**/
+		
+		public static Processing getFromValue(String value) {
+	        switch (value) {
+	        case PROCESSING_VALUE_EXPENDITURES_IMPORTATION: return Processing.EXPENDITURES_IMPORTATION;
+	        case PROCESSING_VALUE_DUPLICATION: return Processing.DUPLICATION;
+	        case PROCESSING_VALUE_COPY: return Processing.COPY;
+	        default: throw new IllegalArgumentException(String.format("value [%s] not supported.", value) );
+	        }
+	    }
+	}
 }
