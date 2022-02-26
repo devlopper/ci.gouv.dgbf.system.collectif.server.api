2a3d82d84a50c1b3c08be3146323cf1fe26c18dd
devlopper <kycdev@gmail.com>
2022-02-26 11:09:36 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/business/RegulatoryActBusiness.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/business/RegulatoryActBusiness.java
@@ -9,11 +9,14 @@ import ci.gouv.dgbf.system.collectif.server.api.persistence.RegulatoryAct;
 
 public interface RegulatoryActBusiness extends SpecificBusiness<RegulatoryAct> {
 
+	String INCLUDE_BY_LEGISLATIVE_ACT_IDENTIFIER_AUDIT_IDENTIFIER = "INCLUSION_PAR_VERSION_COLLECTIF";
+	Result includeByLegislativeActVersionIdentifier(String legislativeActVersionIdentifier,String auditWho);
+	
 	String INCLUDE_AUDIT_IDENTIFIER = "INCLUSION";
-	Result include(Collection<String> identifiers,String legislativeActVersionIdentifier,Boolean existingIgnorable,String userIdentifier);
-	Result include(String legislativeActIdentifier,Boolean existingIgnorable,String userIdentifier,String...identifiers);
+	Result include(Collection<String> identifiers,String legislativeActVersionIdentifier,Boolean existingIgnorable,String auditWho);
+	Result include(String legislativeActIdentifier,Boolean existingIgnorable,String auditWho,String...identifiers);
 	
 	String EXCLUDE_AUDIT_IDENTIFIER = "EXCLUSION";
-	Result exclude(Collection<String> identifiers,String legislativeActVersionIdentifier,Boolean existingIgnorable,String userIdentifier);
-	Result exclude(String legislativeActIdentifier,Boolean existingIgnorable,String userIdentifier,String...identifiers);
+	Result exclude(Collection<String> identifiers,String legislativeActVersionIdentifier,Boolean existingIgnorable,String auditWho);
+	Result exclude(String legislativeActIdentifier,Boolean existingIgnorable,String auditWho,String...identifiers);
 }
--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/service/RegulatoryActDto.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/service/RegulatoryActDto.java
@@ -8,12 +8,14 @@ public interface RegulatoryActDto extends IdentifiableSystemScalarStringIdentifi
 	String JSON_CODE = "code";
 	String JSON_NAME = "libelle";
 	String JSON_YEAR = "exercice";
+	String JSON_DATE_AS_STRING = "date_chaine";
 	String JSON_ENTRY_AUTHORIZATION_AMOUNT = "autorisation_engagement";
 	String JSON_PAYMENT_CREDIT_AMOUNT = "credit_paiement";
 	String JSON_INCLUDED = "inclus";
 	String JSON_INCLUDED_AS_STRING = "inclus_chaine";
 	
 	String JSONS_YEAR_NAME_ENTRY_AUTHORIZATION_AMOUNT_PAYMENT_CREDIT_AMOUNT_INCLUDED_AS_STRING = "exercice_libelle_autorisation_engagement_credit_paiement_inclus_chaine";
+	String JSONS_CODE_NAME_YEAR_DATE_AS_STRING_ENTRY_AUTHORIZATION_AMOUNT_PAYMENT_CREDIT_AMOUNT_INCLUDED_AND_INCLUDED_AS_STRING = "code_libelle_exercice_date_chaine_autorisation_engagement_credit_paiement_inclus_chaine";
 	
 	String JSONS_IDENTIFIERS = "identifiants";
 	String JSON_LEGISLATIVE_VERSION_IDENTIFIER = "identifiant_version_collectif";
