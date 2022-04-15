275e35296bc98e258f4ecfb1ad0387b676751569
devlopper <kycdev@gmail.com>
2022-04-15 07:20:45 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/api/service/CommonDto.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/api/service/CommonDto.java
@@ -2,14 +2,20 @@ package ci.gouv.dgbf.system.collectif.server.api.service;
 
 public interface CommonDto {
 	
-	String JSONS_AMOUTNS = "montants";
-	String JSONS_EXPENDITURES_AMOUTNS = "montants_depenses";
-	String JSONS_RESOURCES_AMOUTNS = "montants_ressources";
-	String JSONS_STRINGS = "chaines";
-	
 	String JSON_ENTRY_AUTHORIZATION = "autorisation_engagement";
 	String JSON_PAYMENT_CREDIT = "credit_paiement";
 	String JSON_REVENUE = "revenu";
+	
+	String JSONS_AMOUTNS = "montants";
+	String JSONS_EXPENDITURES_AMOUTNS = "montants_depenses";
+	String JSONS_EXPENDITURES_AMOUTNS_WITHOUT_AVAILABLE = "montants_depenses_sans_disponible";
+	String JSONS_EXPENDITURES_AMOUTNS_WITHOUT_INCLUDED_MOVEMENT_AND_AVAILABLE = "montants_depenses_sans_mouvement_inclus_et_disponible";
+	String JSONS_EXPENDITURES_AMOUTNS_WITH_AVAILABLE_ONLY = "montants_depenses_avec_disponible_seulement";
+	String JSONS_EXPENDITURES_AMOUTNS_WITH_INCLUDED_MOVEMENT_ONLY = "montants_depenses_avec_mouvement_inclus_seulement";
+	String JSONS_EXPENDITURES_ENTRY_AUTHORIZATION_AMOUTNS = "montants_"+JSON_ENTRY_AUTHORIZATION;
+	String JSONS_EXPENDITURES_PAYMENT_CREDIT_AMOUTNS = "montants_"+JSON_PAYMENT_CREDIT;
+	String JSONS_RESOURCES_AMOUTNS = "montants_ressources";
+	String JSONS_STRINGS = "chaines";
 	
 	/* Audits */
 	String JSON___AUDIT__ = "__audit__";
